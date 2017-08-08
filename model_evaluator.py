428ffa74acd187eb73bf3a5c30357718cd8c7614
Peter Yu <yukw777@outlook.com>
2017-08-08 17:07:28 -0400

    ModelEvaluator now performs grid search

--- model_evaluator.py
+++ model_evaluator.py
@@ -1,20 +1,28 @@
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 
 class ModelEvaluator():
 
-    def __init__(self, model, train, label, default_params_for_cv={}):
+    def __init__(self, model, train, label, default_model_params={}):
         self.model = model
         self.train = train
         self.label = label
-        self.default_params_for_cv = default_params_for_cv
+        self.default_model_params = default_model_params
 
     def cross_validate(self, scoring="neg_mean_squared_error", **kwargs):
-        m = self.model(**self.default_params_for_cv)
+        m = self.model(**self.default_model_params)
         self.scores = cross_val_score(
             m, self.train, y=self.label, scoring=scoring, **kwargs)
         self.rmse_scores = np.sqrt(-self.scores)
+
+    def grid_search(self, param_grid,
+                    scoring='neg_mean_squared_error', **kwargs):
+        m = self.model(**self.default_model_params)
+        self.grid_search = GridSearchCV(
+            m, param_grid, scoring=scoring, **kwargs)
+        self.grid_search.fit(self.train, self.label)
 
     def display_rmse_scores(self):
         print("Scores:", self.rmse_scores)
